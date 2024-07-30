3cd369d609b57ee3ebbb32fc65335e154df65e5d
pg.nathania@gmail.com <patricia.grace.nathania-2021@fst.unair.ac.id>
2024-07-30 17:45:17 +0700

    part 3

---
+++ react3-tic-tac-toe/src/App.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+
+function Square({value, onSquareClick}) { 
+  return (
+  <button className="square" onClick={onSquareClick}> 
+  {value} 
+  </button>
+  )
+}
+
+export default function Board() {   
+
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
+  function handleClick (i){
+    // membuat array baru yg duplikatnya
+     const nextSquares = squares.slice()
+     //ganti array baru sesuai dengan tombol yg kita klik -> {'X, null, null, null, null, null, null, null, null }
+     nextSquares[i]= 'X' 
+     setSquares(nextSquares);
+  }
+  return (
+    <div className='board'>
+    <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+    <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+    <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+    <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+    <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+    <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+    <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+    <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+    <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+    </div>
+  )
+}
+
