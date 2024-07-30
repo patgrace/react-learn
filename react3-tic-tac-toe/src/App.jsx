d11e0dff9fb4d8bbdfac0811ca7bfb1687de050e
pg.nathania@gmail.com <patricia.grace.nathania-2021@fst.unair.ac.id>
2024-07-30 18:18:59 +0700

    part 3 done

--- react3-tic-tac-toe/src/App.jsx
+++ react3-tic-tac-toe/src/App.jsx
@@ -13,15 +13,38 @@ function Square({value, onSquareClick}) {
 export default function Board() {   
 
   const [squares, setSquares] = useState(Array(9).fill(null))
+  const [xIsNext, setXIsNext] = useState(true)
+
 
   function handleClick (i){
-    // membuat array baru yg duplikatnya
+     if ( squares[i] || calculateWinner(squares)){
+      return;
+     }
+
+
      const nextSquares = squares.slice()
-     //ganti array baru sesuai dengan tombol yg kita klik -> {'X, null, null, null, null, null, null, null, null }
-     nextSquares[i]= 'X' 
+     if (xIsNext){
+      nextSquares[i] = 'X'
+     } else {
+       nextSquares[i]= 'O' 
+     }
      setSquares(nextSquares);
+     setXIsNext(!xIsNext);
   }
+
+  const winner = calculateWinner(squares);
+  let status = '';
+  if (winner){
+    status = 'Winner:' + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? 'X' : 'O');
+  }
+  
   return (
+    <>
+    <div >
+      {status}
+    </div>
     <div className='board'>
     <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
     <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -33,6 +56,30 @@ export default function Board() {
     <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
     <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
     </div>
+    </>
   )
+}
+
+function calculateWinner (squares){
+  const  rules  = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+
+  for (let i = 0; i < rules.length; i++ ){
+    const [a,b,c] = rules [i];
+
+    if (squares[a] && squares [a] === squares[b] && squares [c]){
+      return squares [a];
+    }
+  }
+
+  return false;
 }
 
